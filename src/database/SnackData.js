9fc0ff3d9a549f2e48edc809bb902fbd32f7aba0
Shaheen Siddiqui <shanvisiddya@gmail.com>
2023-07-21 19:22:36 +0530

    feat: snack table data, to show on UI

---
+++ src/database/SnackData.js
@@ -0,0 +1,39 @@
+export const snacksTableData = [
+  {
+    id: 1,
+    product_name: "Granola Bar",
+    product_weight: "21g",
+    price: 299,
+    calories: 150,
+    ingredients: ["Oats", "Honey", "Nuts", "Dried Fruits"],
+  },
+  {
+    id: 2,
+    product_name: "Fruit and Nut Mix",
+    product_weight: "73g",
+    price: 749,
+    calories: 353,
+    ingredients: [
+      "Almonds",
+      "Cashews",
+      "Dried Cranberries",
+      "Dried Blueberries",
+    ],
+  },
+  {
+    id: 3,
+    product_name: "Veggie Chips",
+    product_weight: "28g",
+    price: 279,
+    calories: 130,
+    ingredients: ["Sweet Potatoes", "Beets", "Kale", "Sea Salt"],
+  },
+  {
+    id: 4,
+    product_name: "Protein Balls",
+    product_weight: "100g",
+    price: 499,
+    calories: 318,
+    ingredients: ["Dates", "Almond Butter", "Protein Powder", "Chia Seeds"],
+  },
+];
