e9a82063d4d600551437d7bbb83c7c4fc94a5cfd
Shaheen Siddiqui <shanvisiddya@gmail.com>
2023-07-22 15:37:31 +0530

    refactor: added more data for better UI

--- src/database/SnackData.js
+++ src/database/SnackData.js
@@ -124,5 +124,183 @@ export const snacksTableData = [
     price: 349,
     calories: 160,
     ingredients: ["Mango", "Citric Acid"],
-  },
+  },{
+    id: 15,
+    product_name: "Mixed Berries",
+    product_weight: "200g",
+    price: 399,
+    calories: 120,
+    ingredients: ["Strawberries", "Blueberries", "Raspberries"],
+    },
+    
+    {
+    id: 16,
+    product_name: "Peanut Butter Cookies",
+    product_weight: "150g",
+    price: 249,
+    calories: 180,
+    ingredients: ["Peanut Butter", "Sugar", "Egg", "Flour"],
+    },
+    
+    {
+    id: 17,
+    product_name: "Green Tea",
+    product_weight: "50g",
+    price: 99,
+    calories: 0,
+    ingredients: ["Green Tea Leaves"],
+    },
+    
+    {
+    id: 18,
+    product_name: "Almond Milk",
+    product_weight: "1L",
+    price: 199,
+    calories: 30,
+    ingredients: ["Almonds", "Water"],
+    },
+    
+    {
+    id: 19,
+    product_name: "Chocolate Chip Muffins",
+    product_weight: "180g",
+    price: 299,
+    calories: 240,
+    ingredients: ["Flour", "Sugar", "Butter", "Chocolate Chips"],
+    },
+    
+    {
+    id: 20,
+    product_name: "Coconut Water",
+    product_weight: "500ml",
+    price: 79,
+    calories: 45,
+    ingredients: ["Coconut Water"],
+    },
+    
+    {
+    id: 21,
+    product_name: "Cheese Crackers",
+    product_weight: "100g",
+    price: 149,
+    calories: 110,
+    ingredients: ["Cheese", "Flour", "Butter"],
+    },
+    
+    {
+    id: 22,
+    product_name: "Mint Lemonade",
+    product_weight: "350ml",
+    price: 129,
+    calories: 70,
+    ingredients: ["Lemon Juice", "Mint Leaves", "Sugar", "Water"],
+    },
+    
+    {
+    id: 23,
+    product_name: "Caramel Popcorn",
+    product_weight: "120g",
+    price: 199,
+    calories: 180,
+    ingredients: ["Popcorn", "Caramel Sauce"],
+    },
+    
+    {
+    id: 24,
+    product_name: "Cherry Tomatoes",
+    product_weight: "250g",
+    price: 89,
+    calories: 25,
+    ingredients: ["Cherry Tomatoes"],
+    },
+    
+    {
+    id: 25,
+    product_name: "Honey Roasted Cashews",
+    product_weight: "120g",
+    price: 249,
+    calories: 200,
+    ingredients: ["Cashews", "Honey", "Salt"],
+    },
+    
+    {
+    id: 26,
+    product_name: "Spinach and Feta Wrap",
+    product_weight: "200g",
+    price: 349,
+    calories: 320,
+    ingredients: ["Spinach", "Feta Cheese", "Whole Wheat Tortilla"],
+    },
+    
+    {
+    id: 27,
+    product_name: "Frozen Blueberries",
+    product_weight: "500g",
+    price: 279,
+    calories: 90,
+    ingredients: ["Blueberries"],
+    },
+    
+    {
+    id: 28,
+    product_name: "Hummus",
+    product_weight: "250g",
+    price: 169,
+    calories: 70,
+    ingredients: ["Chickpeas", "Tahini", "Olive Oil"],
+    },
+    
+    {
+    id: 29,
+    product_name: "Pomegranate Juice",
+    product_weight: "1L",
+    price: 249,
+    calories: 120,
+    ingredients: ["Pomegranate Juice"],
+    },
+    
+    {
+    id: 30,
+    product_name: "Greek Yogurt",
+    product_weight: "500g",
+    price: 199,
+    calories: 140,
+    ingredients: ["Yogurt"],
+    },
+    
+    {
+    id: 31,
+    product_name: "Orange Slices",
+    product_weight: "200g",
+    price: 129,
+    calories: 80,
+    ingredients: ["Orange Slices"],
+    },
+    
+    {
+    id: 32,
+    product_name: "Pistachio Ice Cream",
+    product_weight: "500ml",
+    price: 349,
+    calories: 280,
+    ingredients: ["Milk", "Sugar", "Pistachios"],
+    },
+    
+    {
+    id: 33,
+    product_name: "Quinoa Salad",
+    product_weight: "300g",
+    price: 299,
+    calories: 200,
+    ingredients: ["Quinoa", "Cucumber", "Tomatoes", "Feta Cheese"],
+    },
+    
+    {
+    id: 34,
+    product_name: "Trail Mix",
+    product_weight: "150g",
+    price: 249,
+    calories: 180,
+    ingredients: ["Almonds", "Raisins", "Cashews", "Dried Cranberries"],
+    },
 ];
