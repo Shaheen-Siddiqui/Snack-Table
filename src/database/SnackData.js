0e6d7e4f49c3d0f9364d346bb320e966957bbaa8
Shaheen Siddiqui <shanvisiddya@gmail.com>
2023-07-22 15:28:17 +0530

    feat: increase snack table length for pagination funtion

--- src/database/SnackData.js
+++ src/database/SnackData.js
@@ -36,4 +36,93 @@ export const snacksTableData = [
     calories: 318,
     ingredients: ["Dates", "Almond Butter", "Protein Powder", "Chia Seeds"],
   },
+  {
+    id: 5,
+    product_name: "Trail Mix",
+    product_weight: "150g",
+    price: 349,
+    calories: 200,
+    ingredients: ["Peanuts", "Raisins", "Cashews", "Chocolate Chips"],
+  },
+
+  {
+    id: 6,
+    product_name: "Granola Bites",
+    product_weight: "80g",
+    price: 249,
+    calories: 180,
+    ingredients: ["Oats", "Honey", "Peanut Butter", "Dried Cranberries"],
+  },
+
+  {
+    id: 7,
+    product_name: "Kale Chips",
+    product_weight: "50g",
+    price: 199,
+    calories: 120,
+    ingredients: ["Kale", "Olive Oil", "Sea Salt"],
+  },
+
+  {
+    id: 8,
+    product_name: "Fruit Leather",
+    product_weight: "40g",
+    price: 149,
+    calories: 90,
+    ingredients: ["Apples", "Strawberries", "Lemon Juice"],
+  },
+
+  {
+    id: 9,
+    product_name: "Roasted Chickpeas",
+    product_weight: "120g",
+    price: 299,
+    calories: 220,
+    ingredients: ["Chickpeas", "Olive Oil", "Paprika", "Garlic Powder"],
+  },
+
+  {
+    id: 10,
+    product_name: "Rice Crackers",
+    product_weight: "100g",
+    price: 249,
+    calories: 160,
+    ingredients: ["Rice Flour", "Soy Sauce", "Sesame Seeds"],
+  },
+
+  {
+    id: 11,
+    product_name: "Popcorn",
+    product_weight: "80g",
+    price: 199,
+    calories: 100,
+    ingredients: ["Corn Kernels", "Butter", "Salt"],
+  },
+
+  {
+    id: 12,
+    product_name: "Seaweed Snacks",
+    product_weight: "30g",
+    price: 149,
+    calories: 50,
+    ingredients: ["Seaweed", "Sesame Oil", "Salt"],
+  },
+
+  {
+    id: 13,
+    product_name: "Mixed Nuts",
+    product_weight: "200g",
+    price: 399,
+    calories: 280,
+    ingredients: ["Almonds", "Cashews", "Pistachios", "Walnuts"],
+  },
+
+  {
+    id: 14,
+    product_name: "Dried Mango",
+    product_weight: "120g",
+    price: 349,
+    calories: 160,
+    ingredients: ["Mango", "Citric Acid"],
+  },
 ];
